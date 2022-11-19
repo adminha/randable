45473acc43ecc5299836b21c8949887c3cbbb0ad
ISC.Safarian <ISC.Safarian@cbi.ir>
2022-11-19 11:31:41 +0330

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const randable = (stringLength = 10) => {
+    let randomStr = "";
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+    for (let index = 0; index < stringLength; index++) {
+      randomStr += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+    }
+    return randomStr;
+};
+export default randable;
+  
+  
